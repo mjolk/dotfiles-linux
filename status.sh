4846a61542dcd31525d76074c3be695b22b95eba
mjolk <2mjolk@gmail.com>
2026-02-11 18:10:16 +0100

    swaylock/suspend stuff

--- status.sh
+++ status.sh
@@ -1,20 +1,39 @@
 #!/bin/zsh
+function mobile {
 
-now=$(date +'%D %T');
+now=$(date +'%d %b %T');
 power=$(cat /sys/class/power_supply/BAT0/capacity);
 wifi=$(iwctl station wlan0 show);
 parts=(${(@s/ /)wifi})
+wificonnected=""
 if [[ $parts[12] = "connected" ]]; then
-  wificonnected="\uf1eb  $parts[16]" 
+  wificonnected="\uf0c2  $parts[16]" 
 else
-  wificonnected="\uf2cc "
+  wificonnected="\uebaa "
 fi
-test=$'\uf242'
+powerstr=$'\uf240'
 if (( $power < 60 )); then
-  powerstr=$'\uf243';
-elif (( $power < 40)); then
   powerstr=$'\uf242';
-else
-  powerstr=$'\uf240';
 fi
-echo "$wificonnected $powerstr  $now";
+if (( $power < 40)); then
+  powerstr=$'\uf243';
+fi
+if (( $power < 20)); then
+  powerstr=$'\uf244';
+fi
+echo "$wificonnected $powerstr $power% $now \ueeef  ";
+}
+
+function station {
+
+now=$(date +'%d %b %T');
+music=$(mpc current)
+
+echo "\uf001 $music $now";
+}
+
+if [ "$1" = "station" ]; then
+  station
+else
+  mobile
+fi
