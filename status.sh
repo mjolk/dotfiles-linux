fc5ac77cd2399534540d2c52e8ef6735d90b70e4
mjolk <2mjolk@gmail.com>
2026-03-29 23:07:32 +0200

    remove stupid stuff, just use playerctl --format, No players found stderr message is a bug that's been open for 2 years....

--- status.sh
+++ status.sh
@@ -13,11 +13,7 @@ function music() {
 function mobile() {
 
 now=$(date +'%d %b %T');
-music=$(playerctl metadata)
-parts=(${(ps:\n:)music})
-sparts=(${(ps:  :)parts[4]})
-ssparts=(${(ps:  :)parts[5]})
-sssparts=(${(ps:  :)parts[6]})
+music=$(playerctl metadata --format "playing: {{ artist }} {{ album }} {{ title }}");
 power=$(cat /sys/class/power_supply/BAT0/capacity);
 wifi=$(iwctl station wlan0 show);
 parts=(${(@s/ /)wifi})
@@ -37,39 +33,14 @@ fi
 if (( $power < 20)); then
   powerstr=$'\uf244';
 fi
-echo "\uf001 $ssparts[2]$sssparts[2]$sparts[2] $wificonnected $powerstr $power% $now \ueeef  ";
+echo "\uf001 $music $wificonnected $powerstr $power% $now \ueeef  ";
 }
 
 function station() {
 
 now=$(date +'%d %b %T');
-music=$(playerctl metadata)
-parts=(${(ps:\n:)music})
-artist=""
-album=""
-title=""
-for i in {1..${#parts[@]}}; do
-  #echo "partt: ${parts[i]}\n"
-  if [ "${parts[i]#*album}" != "$parts[i]" ]; then
-    #echo "found album part $parts[i]"
-    pp=(${(ps:    :)parts[i]})
-    album="${pp[@]: -1}"
-    album=${${album%${album##*[^[:blank:]]}}#${${album%${album##*[^[:blank:]]}}%%[^[:blank:]]*}}
-  fi
-  if [ "${parts[i]#*artist}" != "$parts[i]" ]; then
-    #echo "found artist part $parts[i]"
-    pp=(${(ps:    :)parts[i]})
-    artist="${=${pp[@]: -1}}"
-    artist=${${artist%${artist##*[^[:blank:]]}}#${${artist%${artist##*[^[:blank:]]}}%%[^[:blank:]]*}}
-  fi
-  if [ "${parts[i]#*title}" != "$parts[i]" ]; then
-    #echo "found title part $parts[i]"
-    pp=(${(ps:    :)parts[i]})
-    title="${pp[@]: -1}"
-    title=${${title%${title##*[^[:blank:]]}}#${${title%${title##*[^[:blank:]]}}%%[^[:blank:]]*}}
-  fi
-done
-echo "\uf001 $artist $title $now";
+music=$(playerctl metadata --format "playing: {{ artist }} {{ album }} {{ title }}");
+echo "\uf001 $music $now";
 
 }
 
